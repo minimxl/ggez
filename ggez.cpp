71bb8b99fb23eae7e00de03b302f8b95eb58823a
Minimxl <minimxlcontact@gmail.com>
2018-03-30 05:55:53 -0400

    General improvements.

--- ggez.cpp
+++ ggez.cpp
@@ -1,71 +1,87 @@
-// GGEZ 1.0 minimxl 3-30-18
+// GGEZ 1.1 minimxl 3-30-18
 // Use: Quickly building and executing apps.
 // Note: Made in a hacky way.
 
 #include <iostream>
 #include <string>
 #include <fstream>
-#include <unistd.h>
-using namespace std;
+#include <thread>
+#include <chrono>
 
 void checkHelp() {
-  cout << "GGEZ is a simple command line tool meant" << endl;
-  cout << "for allowing quick and easy building and" << endl;
-  cout << "execution of applications for those new" << endl;
-  cout << "to C++ programming." << endl;
-  cout << endl;
-  cout << "Usage: ggez <nameOfFileWithoutExtension>" << endl;
+  std::cout << "GGEZ is a simple command line tool meant" << std::endl;
+  std::cout << "for allowing quick and easy building and" << std::endl;
+  std::cout << "execution of applications for those new" << std::endl;
+  std::cout << "to C++ programming." << std::endl;
+  std::cout << std::endl;
+  std::cout << "Usage: ggez <nameOfFileWithoutExtension>" << std::endl;
 }
 
 
-int checkFile(int argc, char* argv[]) {
+int checkFile(char* argv[]) {
   // Add .cpp to the arg to reduce arg typing time.
-  string fname = string(argv[1]) + ".cpp";
+  std::string fname = std::string(argv[1]) + ".cpp";
   // Assure file exists.
-  ifstream file(fname);
+  std::ifstream file(fname);
   if (!file) {
   // If it doesn't exist, exit.
-    cout << "That file doesn't exist. :(" << endl;
+    std::cout << "That file doesn't exist. :(" << std::endl;
     exit(EXIT_FAILURE);
   }
+  return 0;
 }
 
-int buildFile(int argc, char* argv[]) {
-  // Build file and store output in variable.
-  int buildStatus = system((string("g++ ") + argv[1] + (".cpp -o ") + argv[1] + (".o")).c_str());
+int buildFile(char* argv[]) {
+  int buildStatus;
+  std::string warnDecision;
 
+  std::cout << "Use warnings? (Y/N) ";
+  std::cin >> warnDecision;
+
+  if (warnDecision == "Y") {
+    // Build file with warnings and store output in variable.
+    buildStatus = system((std::string("g++ -Wall -Wextra -Wpedantic ") + argv[1] + (".cpp -o ") + argv[1] + (".o")).c_str());
+  } else if (warnDecision == "N") {
+      // Build file and store output in variable.
+      buildStatus = system((std::string("g++ ") + argv[1] + (".cpp -o ") + argv[1] + (".o")).c_str());
+    } else {
+          std::cout << "Invalid input. :(" << std::endl;
+        exit(EXIT_FAILURE);
+      }
   if (!buildStatus) {
     // If no output was recieved, all is good.
-    cout << "GGEZ built new file." << endl;
+      std::cout << "GGEZ built new file." << std::endl;
   } else {
       // Otherwise, an error occured, exit.
-      cout << "There was an issue. :(" << endl;
+        std::cout << "There was an issue. :(" << std::endl;
       exit(EXIT_FAILURE);
     }
+  return 0;
 }
 
-int executeFile(int argc, char* argv[]) {
-    cout << "GGEZ is executing new file: " << endl;
+int executeFile(char* argv[]) {
+    std::cout << "GGEZ is executing new file: " << std::endl;
   // Sleep for added executing suspense, haha.
-  sleep(2);
-    cout << endl;
-  system((string("./") + argv[1] + (".o")).c_str());
-    cout << endl;
-    cout << "GGEZ executed new file." << endl;
+  std::this_thread::sleep_for(std::chrono::seconds(2));
+    std::cout << std::endl;
+  system((std::string("./") + argv[1] + (".o")).c_str());
+    std::cout << std::endl;
+    std::cout << "GGEZ executed new file." << std::endl;
+  return 0;
 }
 
 int main(int argc, char* argv[]) {
   if (!argv[1]){
     // If the user inputs no arg.
-    cout << "For help use: ggez -h." << endl;
-} else if (string(argv[1]) == "-h") {
+    std::cout << "For help use: ggez -h." << std::endl;
+} else if (std::string(argv[1]) == "-h") {
     // If the user asks for help.
     checkHelp();
 } else {
     // If the user uses the app usage designated.
-    checkFile(argc, argv);
-    buildFile(argc, argv);
-    executeFile(argc, argv);
+    checkFile(argv);
+    buildFile(argv);
+    executeFile(argv);
   }
   return 0;
 }
